88fec67138ce2d2b0921c989206574afec43e07a
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2019-06-14 03:06:59 -0600

    Trying using relation groups

--- src/scripts/bio_creative_relation_extraction.clj
+++ src/scripts/bio_creative_relation_extraction.clj
@@ -31,18 +31,33 @@
 (def word2vec-db (io/file word-vector-dir "bio-word-vectors-clj.vec"))
 
 (def factory thal-native/native-double)
-(def properties #{"PART-OF"
-                  "REGULATOR" "DIRECT-REGULATOR" "INDIRECT-REGULATOR"
-                  "UPREGULATOR" "ACTIVATOR" "INDIRECT-UPREGULATOR"
+(def properties #{"UPREGULATOR" "ACTIVATOR" "INDIRECT-UPREGULATOR"
                   "DOWNREGULATOR" "INHIBITOR" "INDIRECT-DOWNREGULATOR"
                   "AGONIST" "AGONIST-ACTIVATOR" "AGONIST-INHIBITOR"
                   "ANTAGONIST"
-                  "MODULATOR" "MODULATOR‐ACTIVATOR" "MODULATOR‐INHIBITOR"
-                  "COFACTOR"
                   "SUBSTRATE" "PRODUCT-OF" "SUBSTRATE_PRODUCT-OF"
-                  "NOT"
                   re-model/NONE}
+  #_#{"PART-OF"
+      "REGULATOR" "DIRECT-REGULATOR" "INDIRECT-REGULATOR"
+      "UPREGULATOR" "ACTIVATOR" "INDIRECT-UPREGULATOR"
+      "DOWNREGULATOR" "INHIBITOR" "INDIRECT-DOWNREGULATOR"
+      "AGONIST" "AGONIST-ACTIVATOR" "AGONIST-INHIBITOR"
+      "ANTAGONIST"
+      "MODULATOR" "MODULATOR‐ACTIVATOR" "MODULATOR‐INHIBITOR"
+      "COFACTOR"
+      "SUBSTRATE" "PRODUCT-OF" "SUBSTRATE_PRODUCT-OF"
+      "NOT"
+      re-model/NONE}
   #_#{"INHIBITOR" #_re-model/NONE})
+
+(def property-map {"UPREGULATOR"   "CPR:3" "ACTIVATOR" "CPR:3" "INDIRECT-UPREGULATOR" "CPR:3"
+                   "DOWNREGULATOR" "CPR:4" "INHIBITOR" "CPR:4" "INDIRECT-DOWNREGULATOR" "CPR:4"
+                   "AGONIST"       "CPR:5" "AGONIST-ACTIVATOR" "CPR:5" "AGONIST-INHIBITOR" "CPR:5"
+                   "ANTAGONIST"    "CPR:6"
+                   "SUBSTRATE"     "CPR:9" "PRODUCT-OF" "CPR:9" "SUBSTRATE_PRODUCT-OF" "CPR:9"
+                   re-model/NONE   re-model/NONE})
+
+(def properties (set (vals property-map)))
 
 ;;; MODELS ;;;
 
@@ -50,21 +65,32 @@
 (rdr/read-biocreative-files training-dir training-pattern training-knowtator-view)
 (def base-training-model (re-model/make-model training-knowtator-view factory word2vec-db))
 (def training-model-with-sentences (assoc base-training-model :sentences (re-model/make-sentences base-training-model)))
-(def training-model (assoc training-model-with-sentences :properties properties))
+(def training-model (assoc (update training-model-with-sentences :sentences
+                                   (fn [sentences]
+                                     (map #(update % :property (fn [property] (or (get property-map property)
+                                                                                  re-model/NONE)))
+                                          sentences)))
+                      :properties properties))
 
 (def testing-knowtator-view (k/model testing-dir nil))
 (rdr/read-biocreative-files testing-dir testing-pattern testing-knowtator-view)
 (def base-testing-model (re-model/make-model testing-knowtator-view factory word2vec-db))
 (def testing-model-with-sentences (assoc base-testing-model :sentences (re-model/make-sentences base-testing-model)))
-(def testing-model (assoc testing-model-with-sentences :properties properties))
+
+(def testing-model (assoc (update testing-model-with-sentences :sentences
+                                  (fn [sentences]
+                                    (map #(update % :property (fn [property] (or (get property-map property)
+                                                                                 re-model/NONE)))
+                                         sentences)))
+                     :properties properties))
 
 ;; This allows me to reset sentences if they get reloaded
-(def training-model (update training-model
-                            :sentences (fn [sentences]
-                                         (map #(re-model/map->Sentence %) sentences))))
-(def testing-model (update testing-model
-                           :sentences (fn [sentences]
-                                        (map #(re-model/map->Sentence %) sentences))))
+#_(def training-model (update training-model
+                              :sentences (fn [sentences]
+                                           (map #(re-model/map->Sentence %) sentences))))
+#_(def testing-model (update testing-model
+                             :sentences (fn [sentences]
+                                          (map #(re-model/map->Sentence %) sentences))))
 
 ;;; SENTENCE STATS ;;;
 (log/info "Model\n"
@@ -85,9 +111,9 @@
                     [:training :testing])
                (incanter/to-dataset)))
 
-(def training-context-paths-plot (evaluation/plot-context-lengths training-model results-dir "Training %s"))
+#_(def training-context-paths-plot (evaluation/plot-context-lengths training-model results-dir "Training %s"))
 #_(incanter/view training-context-paths-plot)
-(def testing-context-paths-plot (evaluation/plot-context-lengths testing-model results-dir "Test %s"))
+#_(def testing-context-paths-plot (evaluation/plot-context-lengths testing-model results-dir "Test %s"))
 #_(incanter/view testing-context-paths-plot)
 
 ;;; CLUSTERING ;;;
@@ -103,8 +129,8 @@
 ;;; RELATION EXTRACTION ;;;
 
 (def prepared-model (let [negatives (filter #(= re-model/NONE (:property %)) (:sentences training-model))
-                          others (remove #(= re-model/NONE (:property %)) (:sentences training-model))
-                          seeds (lazy-cat others (take 200 negatives))]
+                          others    (remove #(= re-model/NONE (:property %)) (:sentences training-model))
+                          seeds    ` (lazy-cat others (take 2000 negatives))]
                       (-> training-model
                           (assoc :s 1
                                  :rng 0.022894)
@@ -112,25 +138,25 @@
                           #_(re-model/split-train-test)
                           (re-model/train-test testing-model))))
 
-#_(def results (-> prepared-model
-                   (assoc :context-path-length-cap 100
-                          :context-thresh 0.95
-                          :cluster-thresh 0.95
-                          :min-match-support 0
-                          :max-iterations 100
-                          :max-matches 3000
-                          :re-clustering? true)
-                   (evaluation/run-model results-dir)))
+(def results (-> prepared-model
+                 (assoc :context-path-length-cap 10
+                        :context-thresh 0.95
+                        :cluster-thresh 0.95
+                        :min-match-support 0
+                        :max-iterations 100
+                        :max-matches 3000
+                        :re-clustering? true)
+                 (evaluation/run-model results-dir)))
 #_(incanter/view (:plot results))
 
 #_(util/map-kv count (group-by :property (:seeds results)))
 
 #_(apply evaluation/format-matches base-training-model results)
 
-(def param-walk-results (evaluation/parameter-walk training-model testing-model results-dir
-                                                   {:context-path-length-cap [100] #_[2 3 5 10 20 35 100]
-                                                    :context-thresh          #_[0.95] [0.975 0.95 0.925 0.9 0.85]
-                                                    :cluster-thresh          #_[0.95] [0.95 0.9 0.75 0.5]
-                                                    :min-match-support       [0] #_[0 5 25]
-                                                    :seed-frac #_[0.2]       [0.05 0.25 0.5 0.75]
-                                                    :rng                     0.022894}))
+#_(def param-walk-results (evaluation/parameter-walk training-model testing-model results-dir
+                                                     {:context-path-length-cap          [100] #_[2 3 5 10 20 35 100]
+                                                      :context-thresh          #_[0.95] [0.975 0.95 0.925 0.9 0.85]
+                                                      :cluster-thresh          #_[0.95] [0.95 0.9 0.75 0.5]
+                                                      :min-match-support                [0] #_[0 5 25]
+                                                      :seed-frac #_[0.2]                [0.05 0.25 0.5 0.75]
+                                                      :rng                              0.022894}))
